2ee2fa74fd28efdd57b848bf5d869f2fe4dcfed8
EduardoGabrielWehrmeister <dudu245@live.com>
2019-05-31 15:21:20 -0300

    Fechado a conexão no método de obter pelo id

--- Repositorio/FilmeRepositorio.cs
+++ Repositorio/FilmeRepositorio.cs
@@ -78,6 +78,7 @@ namespace Repositorio
 
             DataTable dataTable = new DataTable();
             dataTable.Load(comando.ExecuteReader());
+            conexao.Close();
             if (dataTable.Rows.Count == 1)
             {
                 DataRow linha = dataTable.Rows[0];
